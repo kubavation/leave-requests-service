c385579f0f2e7f40992016d95c6e2e2205f69e22
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-07 23:47:48 +0200

    init

---
+++ src/main/kotlin/com/durys/jakub/leaverequests/LeaveRequestsServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.durys.jakub.leaverequests
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeaveRequestsServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LeaveRequestsServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/durys/jakub/leaverequests/LeaveRequestsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.durys.jakub.leaverequests
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LeaveRequestsServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
