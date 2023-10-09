a6e714ced84226ed928d2ed3152026bc768e373a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-09 20:56:58 +0200

    basic implemetntation of RestAcceptantRepository

--- src/main/kotlin/com/durys/jakub/leaverequests/acceptant/domain/AcceptantRepository.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/acceptant/domain/AcceptantRepository.kt
@@ -1,8 +1,9 @@
 package com.durys.jakub.leaverequests.acceptant.domain
 
 import com.durys.jakub.leaverequests.request.domain.LeaveRequest
+import reactor.core.publisher.Mono
 
 internal interface AcceptantRepository {
-    fun load(id: AcceptantId): Acceptant?
-    fun load(request: LeaveRequest): Acceptant?
+    fun load(id: AcceptantId): Mono<Acceptant>
+    fun load(request: LeaveRequest): Mono<Acceptant>
 }
--- src/main/kotlin/com/durys/jakub/leaverequests/acceptant/infastructure/RestAcceptantRepository.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/acceptant/infastructure/RestAcceptantRepository.kt
@@ -5,14 +5,24 @@ import com.durys.jakub.leaverequests.acceptant.domain.AcceptantId
 import com.durys.jakub.leaverequests.acceptant.domain.AcceptantRepository
 import com.durys.jakub.leaverequests.request.domain.LeaveRequest
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import java.time.LocalDate
 
 internal class RestAcceptantRepository(private val client: WebClient): AcceptantRepository {
 
-    override fun load(id: AcceptantId): Acceptant? {
-        TODO("Not yet implemented")
+    override fun load(id: AcceptantId): Mono<Acceptant> {
+        return Mono.empty()
     }
 
-    override fun load(request: LeaveRequest): Acceptant? {
-        TODO("Not yet implemented")
+    override fun load(request: LeaveRequest): Mono<Acceptant> {
+        return client.get()
+                .uri { builder -> builder
+                        .path("/employees/{employeeId}")
+                        .queryParam("at", LocalDate.now())
+                        .queryParam("level", 1) //todo based on request
+                        .build(request.applicantId) }
+                .retrieve()
+                .bodyToMono(Acceptant::class.java)
+                .onErrorResume { Mono.empty() }
     }
 }
