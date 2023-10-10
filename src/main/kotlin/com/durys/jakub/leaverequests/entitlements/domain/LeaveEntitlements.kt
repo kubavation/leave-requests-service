b7d817b7418136b1b7d85f3f8db17ff70bd05c5a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-10 20:34:41 +0200

    additional methods in settlement + fix in entitlements

--- src/main/kotlin/com/durys/jakub/leaverequests/entitlements/domain/LeaveEntitlements.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/entitlements/domain/LeaveEntitlements.kt
@@ -2,5 +2,7 @@ package com.durys.jakub.leaverequests.entitlements.domain
 
 import java.math.BigDecimal
 
-internal class LeaveEntitlements(private val id: LeaveEntitlementsId, private val days: Int, private val hours: BigDecimal){
+internal data class LeaveEntitlements(val id: LeaveEntitlementsId, val days: BigDecimal, val hours: BigDecimal) {
+
+
 }
--- src/main/kotlin/com/durys/jakub/leaverequests/request/Settlement.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/Settlement.kt
@@ -1,7 +1,16 @@
 package com.durys.jakub.leaverequests.request
 
+import com.durys.jakub.leaverequests.request.domain.vo.DailyPeriod
+import com.durys.jakub.leaverequests.request.domain.vo.HourlyPeriod
 import com.durys.jakub.leaverequests.request.domain.vo.Period
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 class Settlement(val period: Period) {
+
+    companion object {
+        fun hourly(from: LocalDateTime, to: LocalDateTime) = Settlement(HourlyPeriod(from, to))
+        fun daily(from: LocalDate, to: LocalDate) = Settlement(DailyPeriod(from, to))
+    }
 
 }
