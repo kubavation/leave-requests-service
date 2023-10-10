2c6ee971d558831960b2bd84dcdc4809296fb37f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-10 20:48:11 +0200

    init of settlementType resolver

--- src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/ApplicantRepository.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/ApplicantRepository.kt
@@ -1,5 +1,5 @@
 package com.durys.jakub.leaverequests.applicant.domain
 
 internal interface ApplicantRepository {
-    fun load(id: ApplicantId): Applicant;
+    fun load(id: ApplicantId): Applicant
 }
---
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/SettlementTypeResolver.kt
@@ -0,0 +1,15 @@
+package com.durys.jakub.leaverequests.request.domain
+
+import com.durys.jakub.leaverequests.applicant.domain.ApplicantId
+import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
+import com.durys.jakub.leaverequests.request.domain.vo.SettlementType
+import org.springframework.stereotype.Service
+
+@Service
+internal class SettlementTypeResolver {
+
+    fun resolve(applicantId: ApplicantId, requestType: LeaveRequestType): SettlementType {
+        //TODO explore domain
+        return SettlementType.DAILY;
+    }
+}
