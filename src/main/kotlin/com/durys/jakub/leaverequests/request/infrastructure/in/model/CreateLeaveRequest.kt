2605fb6cf970588d3c2ea0fe8f53f41827956b21
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-12 19:58:04 +0200

    dto for creating leave request

---
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/infrastructure/in/model/CreateLeaveRequest.kt
@@ -0,0 +1,6 @@
+package com.durys.jakub.leaverequests.request.infrastructure.`in`.model
+
+import java.time.LocalDate
+
+class CreateLeaveRequest(applicantId: String, from: LocalDate, to: LocalDate, alternateId: String?) {
+}
