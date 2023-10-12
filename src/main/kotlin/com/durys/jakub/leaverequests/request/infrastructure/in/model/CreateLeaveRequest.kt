2822cf59f983e79b3f602d8270d5308c6e4f1cb9
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-12 20:47:37 +0200

    additional field in createLeaveRequest

--- src/main/kotlin/com/durys/jakub/leaverequests/request/infrastructure/in/model/CreateLeaveRequest.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/infrastructure/in/model/CreateLeaveRequest.kt
@@ -1,6 +1,8 @@
 package com.durys.jakub.leaverequests.request.infrastructure.`in`.model
 
+import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
 import java.time.LocalDate
 
-class CreateLeaveRequest(val applicantId: String, val from: LocalDate, val to: LocalDate, val alternateId: String?) {
+internal class CreateLeaveRequest(val applicantId: String, val from: LocalDate, val to: LocalDate, val type: LeaveRequestType,
+                         val alternateId: String?) {
 }
