4281a449a3f9ad69d70b1873c5db9719a24ff3e7
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-12 21:43:18 +0200

    usage of status in leaveRequest

--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/LeaveRequest.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/LeaveRequest.kt
@@ -7,4 +7,12 @@ import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
 internal data class LeaveRequest(val requestId: LeaveRequestId, val applicantId: ApplicantId, val settlement: Settlement,
                                  val type: LeaveRequestType, val alternateId: AlternateId? = null) {
 
+    constructor(requestId: LeaveRequestId, applicantId: ApplicantId, settlement: Settlement,
+                type: LeaveRequestType, alternateId: AlternateId?, status: Status)
+                        : this(requestId, applicantId, settlement, type, alternateId) {
+        this.status = status
+    }
+
+    private var status: Status = Status.NEW
+
 }
