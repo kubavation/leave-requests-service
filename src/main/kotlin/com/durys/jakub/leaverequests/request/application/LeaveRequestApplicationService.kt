91c8c3c9f33146a10cd3fac6f6c41a11492ac324
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-12 19:11:32 +0200

    initial implementation of submit in applicationService

--- src/main/kotlin/com/durys/jakub/leaverequests/request/application/LeaveRequestApplicationService.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/application/LeaveRequestApplicationService.kt
@@ -1,6 +1,9 @@
 package com.durys.jakub.leaverequests.request.application
 
 import com.durys.jakub.leaverequests.applicant.domain.ApplicantId
+import com.durys.jakub.leaverequests.common.OperationResult
+import com.durys.jakub.leaverequests.entitlements.domain.LeaveEntitlementsId
+import com.durys.jakub.leaverequests.entitlements.domain.LeaveEntitlementsRepository
 import com.durys.jakub.leaverequests.request.domain.LeaveRequest
 import com.durys.jakub.leaverequests.request.domain.LeaveRequestId
 import com.durys.jakub.leaverequests.request.domain.LeaveRequestRepository
@@ -9,15 +12,17 @@ import com.durys.jakub.leaverequests.request.domain.vo.AlternateId
 import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
 import com.durys.jakub.leaverequests.sharedkernel.identityprovider.IdentityProvider
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
 import java.time.LocalDate
 
 @Component
 internal class LeaveRequestApplicationService(
         private val leaveRequestRepository: LeaveRequestRepository,
+        private val leaveEntitlementsRepository: LeaveEntitlementsRepository,
         private val identityProvider: IdentityProvider) {
 
     internal fun submit(applicantId: ApplicantId, leaveRequestType: LeaveRequestType,
-                        from: LocalDate, to: LocalDate, alternateId: AlternateId?) {
+                        from: LocalDate, to: LocalDate, alternateId: AlternateId?): Mono<OperationResult> {
 
         val leaveRequest = LeaveRequest(
                 LeaveRequestId(identityProvider.next()),
@@ -26,7 +31,18 @@ internal class LeaveRequestApplicationService(
                 leaveRequestType,
                 alternateId)
 
-        leaveRequestRepository.save(leaveRequest)
+        return leaveEntitlementsRepository.load(LeaveEntitlementsId(applicantId, leaveRequestType, LocalDate.now()))
+                .map { it.valid(leaveRequest) }
+                .doOnError { throw RuntimeException("failed to load leave entitlements") }
+                .map {
+                    it.fold(
+                        { error -> OperationResult.error(listOf(error.message!!)) },
+                        { _ ->
+                            leaveRequestRepository.save(leaveRequest)
+                            OperationResult.success()
+                        }
+                    )
+                }
     }
 
 }
