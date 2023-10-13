02d6d404870dae3447cebb02870786dce6224800
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-13 20:12:55 +0200

    sentForAcceptationLeaveRequest

--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/SentForAcceptationLeaveRequest.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/SentForAcceptationLeaveRequest.kt
@@ -4,8 +4,8 @@ import com.durys.jakub.leaverequests.applicant.domain.ApplicantId
 import com.durys.jakub.leaverequests.request.domain.vo.AlternateId
 import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
 
-internal class SubmittedLeaveRequest(private val information: LeaveRequestInformation, private val applicantId: ApplicantId,
-                                     val type: LeaveRequestType, val alternateId: AlternateId? = null): LeaveRequest {
+internal class SentForAcceptationLeaveRequest(private val information: LeaveRequestInformation, private val applicantId: ApplicantId,
+                                              val type: LeaveRequestType, val alternateId: AlternateId? = null): LeaveRequest {
 
 
     override fun information(): LeaveRequestInformation = information
