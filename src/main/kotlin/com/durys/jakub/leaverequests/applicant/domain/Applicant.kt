16b2e5b26c88cead20e486e9232093b3530768af
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-13 20:32:57 +0200

    method for sending for acceptation in applicant root

--- src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/Applicant.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/Applicant.kt
@@ -1,4 +1,15 @@
 package com.durys.jakub.leaverequests.applicant.domain
 
+import com.durys.jakub.leaverequests.acceptant.domain.AcceptantId
+import com.durys.jakub.leaverequests.request.domain.SentForAcceptationLeaveRequest
+import com.durys.jakub.leaverequests.request.domain.SubmittedLeaveRequest
+import java.util.*
+
 internal class Applicant(private val id: ApplicantId) {
+
+
+    fun sentForAcceptation(request: SubmittedLeaveRequest): SentForAcceptationLeaveRequest {
+        val acceptantId = AcceptantId(UUID.randomUUID()) //todo find acceptantId
+        return request.sendForAcceptation(acceptantId)
+    }
 }
