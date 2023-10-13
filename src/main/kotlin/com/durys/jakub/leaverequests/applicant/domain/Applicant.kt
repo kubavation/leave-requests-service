0b71d857a3717341db92ac96199aae2122703d2a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-13 21:21:29 +0200

    build fix + todo

--- src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/Applicant.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/Applicant.kt
@@ -1,6 +1,7 @@
 package com.durys.jakub.leaverequests.applicant.domain
 
 import com.durys.jakub.leaverequests.acceptant.domain.AcceptantId
+import com.durys.jakub.leaverequests.request.domain.*
 import com.durys.jakub.leaverequests.request.domain.LeaveRequestInformation
 import com.durys.jakub.leaverequests.request.domain.SentForAcceptationLeaveRequest
 import com.durys.jakub.leaverequests.request.domain.SubmittedLeaveRequest
@@ -11,7 +12,8 @@ internal class Applicant(private val id: ApplicantId) {
 
 
     fun submit(request: WorkingLeaveRequest): SubmittedLeaveRequest {
-        return SubmittedLeaveRequest(LeaveRequestInformation(request.id, request.type, request.settlement), id, request.alternateId)
+        //todo settlement factory
+        return SubmittedLeaveRequest(LeaveRequestInformation(request.id, request.type, Settlement.daily(request.from, request.to)), id, request.alternateId)
     }
 
     fun sentForAcceptation(request: SubmittedLeaveRequest): SentForAcceptationLeaveRequest {
