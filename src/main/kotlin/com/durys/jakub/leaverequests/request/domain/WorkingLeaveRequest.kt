f1e0da0ea682ac060ab71b29979889e6387c17ff
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-13 21:19:42 +0200

    refactor -> application service

--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/WorkingLeaveRequest.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/WorkingLeaveRequest.kt
@@ -2,7 +2,8 @@ package com.durys.jakub.leaverequests.request.domain
 
 import com.durys.jakub.leaverequests.request.domain.vo.AlternateId
 import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
+import java.time.LocalDate
 
 internal class WorkingLeaveRequest(val id: LeaveRequestId, val type: LeaveRequestType,
-                          val settlement: Settlement, val alternateId: AlternateId? = null) {
+                          val from: LocalDate, val to: LocalDate, val alternateId: AlternateId? = null) {
 }
