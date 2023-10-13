7f570918130cde7993df65b9e909839e00ad5a0c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-13 20:08:04 +0200

    submittedLeaveRequest model

---
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/SubmittedLeaveRequest.kt
@@ -0,0 +1,14 @@
+package com.durys.jakub.leaverequests.request.domain
+
+import com.durys.jakub.leaverequests.applicant.domain.ApplicantId
+import com.durys.jakub.leaverequests.request.domain.vo.AlternateId
+import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
+
+internal class SubmittedLeaveRequest(private val information: LeaveRequestInformation, private val applicantId: ApplicantId,
+                                     val type: LeaveRequestType, val alternateId: AlternateId? = null): LeaveRequest {
+
+
+    override fun information(): LeaveRequestInformation = information
+
+
+}
