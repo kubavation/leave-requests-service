23b853a10f29a8bc630f3921f9ae9800c79b6cb2
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-14 09:05:20 +0200

    rejectedReason vo in RejectedLeaveRequest

--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/RejectedLeaveRequest.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/RejectedLeaveRequest.kt
@@ -1,6 +1,7 @@
 package com.durys.jakub.leaverequests.request.domain
 
-internal class RejectedLeaveRequest(private val information: LeaveRequestInformation): LeaveRequest {
+internal class RejectedLeaveRequest(private val information: LeaveRequestInformation,
+                                    private val rejectionReason: RejectionReason): LeaveRequest {
 
 
     override fun information(): LeaveRequestInformation = information
---
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/RejectionReason.kt
@@ -0,0 +1,4 @@
+package com.durys.jakub.leaverequests.request.domain
+
+internal class RejectionReason(private val value: String) {
+}
