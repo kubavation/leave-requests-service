27b38a18d0a3e5e6a770969df679b5c73c9dbe20
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-15 20:00:18 +0200

    working time schedule model + calculate method

--- src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/workschedule/WorkingTimeSchedule.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/workschedule/WorkingTimeSchedule.kt
@@ -1,5 +1,6 @@
 package com.durys.jakub.leaverequests.applicant.domain.workschedule
 
+import java.math.BigDecimal
 import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalTime
@@ -9,5 +10,19 @@ class WorkingTimeSchedule(val date: LocalDate, val from: LocalTime, val to: Loca
     fun hours() = Duration.between(from, to).toHours()
 
     fun minutes() = Duration.between(from, to).toMinutes()
+
+    companion object {
+
+        fun calculate(schedules: List<WorkingTimeSchedule>): WorkingTimeScheduleAmount {
+
+            val amount = schedules
+                .filter { it.workingDay }
+                .map { Pair(it.hours(), it.minutes()) }
+                .toList()
+
+            return WorkingTimeScheduleAmount(amount.sumOf { BigDecimal.valueOf(it.first) }, amount.sumOf { BigDecimal.valueOf(it.second) })
+        }
+
+    }
 
 }
---
+++ src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/workschedule/WorkingTimeScheduleAmount.kt
@@ -0,0 +1,6 @@
+package com.durys.jakub.leaverequests.applicant.domain.workschedule
+
+import java.math.BigDecimal
+
+class WorkingTimeScheduleAmount(val hours: BigDecimal, val minutes: BigDecimal) {
+}
