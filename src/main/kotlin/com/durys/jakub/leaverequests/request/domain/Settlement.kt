1d0a40001ad0d945d044989b81eff084b2b8e25f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-15 18:47:24 +0200

    changes in submit method

--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/Settlement.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/Settlement.kt
@@ -3,14 +3,16 @@ package com.durys.jakub.leaverequests.request.domain
 import com.durys.jakub.leaverequests.request.domain.vo.DailyPeriod
 import com.durys.jakub.leaverequests.request.domain.vo.HourlyPeriod
 import com.durys.jakub.leaverequests.request.domain.vo.Period
-import java.time.LocalDate
 import java.time.LocalDateTime
 
-class Settlement(val period: Period) {
+class Settlement(private val period: Period) {
 
     companion object {
         fun hourly(from: LocalDateTime, to: LocalDateTime) = Settlement(HourlyPeriod(from, to))
-        fun daily(from: LocalDate, to: LocalDate) = Settlement(DailyPeriod(from, to))
+        fun daily(from: LocalDateTime, to: LocalDateTime) = Settlement(DailyPeriod(from, to))
     }
+
+    fun from(): LocalDateTime = period.from()
+    fun to(): LocalDateTime = period.to()
 
 }
