8700973707682c6a9fd69561f4ac25461147b0c7
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-16 20:41:42 +0200

    settlement repository

--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/settlement/SettlementTypeResolver.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/settlement/SettlementTypeResolver.kt
@@ -2,7 +2,6 @@ package com.durys.jakub.leaverequests.request.domain.settlement
 
 import com.durys.jakub.leaverequests.applicant.domain.ApplicantId
 import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
-import com.durys.jakub.leaverequests.request.domain.vo.SettlementType
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,6 +9,7 @@ internal class SettlementTypeResolver {
 
     fun resolve(applicantId: ApplicantId, requestType: LeaveRequestType): SettlementType {
         //TODO explore domain
+
         return SettlementType.DAILY;
     }
 }
