77fae46ffdc7253b408f9c891583c323103d45b1
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-16 18:25:23 +0200

    period factory - initial implementation

---
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/PeriodFactory.kt
@@ -0,0 +1,38 @@
+package com.durys.jakub.leaverequests.request.domain
+
+import com.durys.jakub.leaverequests.applicant.domain.ApplicantId
+import com.durys.jakub.leaverequests.applicant.domain.workschedule.WorkingTimeSchedule
+import com.durys.jakub.leaverequests.applicant.domain.workschedule.WorkingTimeScheduleRepository
+import com.durys.jakub.leaverequests.request.domain.vo.DailyPeriod
+import com.durys.jakub.leaverequests.request.domain.vo.HourlyPeriod
+import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
+import com.durys.jakub.leaverequests.request.domain.vo.Period
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import java.time.LocalDate
+import java.time.LocalTime
+
+@Component
+internal class PeriodFactory(private val workingTimeScheduleRepository: WorkingTimeScheduleRepository,
+        private val leaveRequestService: LeaveRequestService) {
+
+
+    fun period(applicantId: ApplicantId, requestType: LeaveRequestType, from: LocalDate, to: LocalDate,
+               timeFrom: LocalTime?, timeTo: LocalTime?): Mono<Period> {
+
+        return leaveRequestService.hoursDefinitionRequired(applicantId, requestType, to)
+                .flatMap { createPeriod(applicantId, from, to, it, timeFrom, timeTo) }
+    }
+
+    private fun createPeriod(applicantId: ApplicantId, from: LocalDate, to: LocalDate, hoursDefinitionRequired: Boolean,
+                             timeFrom: LocalTime?, timeTo: LocalTime?): Mono<Period> {
+
+        if (hoursDefinitionRequired) {
+            return Mono.just(HourlyPeriod(from, timeFrom!!, timeTo!!))
+        }
+
+        return WorkingTimeSchedule.calculate(workingTimeScheduleRepository.workingTimeSchedule(applicantId, from, to))
+                .map { DailyPeriod(from, to, it.days, it.hours) }
+    }
+
+}
