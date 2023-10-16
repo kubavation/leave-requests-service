8a6ebbc03ed11aa5929e87cf53be5a21c631a290
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-16 12:13:04 +0200

    changes in period Definition

--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/vo/HourlyPeriod.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/vo/HourlyPeriod.kt
@@ -1,15 +1,16 @@
 package com.durys.jakub.leaverequests.request.domain.vo
 
 import java.math.BigDecimal
-import java.time.LocalDateTime
+import java.time.Duration
+import java.time.LocalDate
+import java.time.LocalTime
 
-internal class HourlyPeriod(private val from: LocalDateTime, private val to: LocalDateTime): Period {
+internal class HourlyPeriod(private val at: LocalDate, private val from: LocalTime, private val to: LocalTime)
+    : Period(at, at, from, to) {
 
+    override fun days() = BigDecimal.ONE
 
-    override fun amount(): BigDecimal = (java.time.Duration.between(from, to).toHours() + 1).toBigDecimal()
+    override fun hours() = Duration.between(from, to).toHours().toBigDecimal()
 
-    override fun from() = from
-
-    override fun to() = to
 
 }
