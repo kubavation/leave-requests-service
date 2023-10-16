9b69fb832100ac3f9130ca3bf03c3871b5e69e87
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-16 21:02:50 +0200

    leaveRequest factory

--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/flow/WorkingLeaveRequest.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/flow/WorkingLeaveRequest.kt
@@ -1,9 +1,11 @@
 package com.durys.jakub.leaverequests.request.domain.flow
 
+import com.durys.jakub.leaverequests.request.domain.settlement.SettlementType
 import com.durys.jakub.leaverequests.request.domain.vo.AlternateId
 import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestId
 import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
 import com.durys.jakub.leaverequests.request.domain.vo.Period
 
-internal class WorkingLeaveRequest(val id: LeaveRequestId, val type: LeaveRequestType, val period: Period, val alternateId: AlternateId? = null) {
+internal class WorkingLeaveRequest(val id: LeaveRequestId, val type: LeaveRequestType, val period: Period,
+                                   val settlementType: SettlementType, val alternateId: AlternateId? = null) {
 }
