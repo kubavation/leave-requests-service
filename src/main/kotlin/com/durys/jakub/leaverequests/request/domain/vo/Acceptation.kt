fb955c7a6ca19bdff8c31c3e91ea4f6b60514c56
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-17 20:14:20 +0200

    acceptation vo

---
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/vo/Acceptation.kt
@@ -0,0 +1,6 @@
+package com.durys.jakub.leaverequests.request.domain.vo
+
+import com.durys.jakub.leaverequests.acceptant.domain.AcceptantId
+
+internal class Acceptation(val acceptantId: AcceptantId, val acceptationLevel: AcceptationLevel) {
+}
---
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/vo/AcceptationLevel.kt
@@ -0,0 +1,3 @@
+package com.durys.jakub.leaverequests.request.domain.vo
+
+internal class AcceptationLevel(private val level: Int)
