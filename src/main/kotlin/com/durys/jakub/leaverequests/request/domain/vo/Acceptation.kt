595573361987d0c750746b7ff41d8ba4bd30f5a3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-17 20:33:14 +0200

    util methods in acceptation / level vo

--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/vo/Acceptation.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/vo/Acceptation.kt
@@ -2,5 +2,12 @@ package com.durys.jakub.leaverequests.request.domain.vo
 
 import com.durys.jakub.leaverequests.acceptant.domain.AcceptantId
 
-internal class Acceptation(val acceptantId: AcceptantId, val acceptationLevel: AcceptationLevel) {
+internal class Acceptation(val acceptantId: AcceptantId?, val acceptationLevel: AcceptationLevel) {
+
+    companion object {
+        fun default() = Acceptation(null, AcceptationLevel(0))
+    }
+
+    fun increment(acceptantId: AcceptantId) = Acceptation(acceptantId, AcceptationLevel(acceptationLevel.value() + 1))
+
 }
--- src/main/kotlin/com/durys/jakub/leaverequests/request/domain/vo/AcceptationLevel.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/domain/vo/AcceptationLevel.kt
@@ -1,3 +1,5 @@
 package com.durys.jakub.leaverequests.request.domain.vo
 
-internal class AcceptationLevel(private val level: Int)
+internal class AcceptationLevel(private val level: Int) {
+    fun value() = level
+}
