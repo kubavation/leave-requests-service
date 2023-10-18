2f951ca1aadfccc1fa628afc43391216cce9d628
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-18 19:44:25 +0200

    publishing event on leaveRequestCancel

--- src/main/kotlin/com/durys/jakub/leaverequests/request/application/handler/CancelLeaveRequestCommandHandler.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/application/handler/CancelLeaveRequestCommandHandler.kt
@@ -3,8 +3,11 @@ package com.durys.jakub.leaverequests.request.application.handler
 import com.durys.jakub.leaverequests.applicant.domain.ApplicantRepository
 import com.durys.jakub.leaverequests.common.OperationResult
 import com.durys.jakub.leaverequests.cqrs.command.CommandHandler
+import com.durys.jakub.leaverequests.cqrs.event.Events
+import com.durys.jakub.leaverequests.request.domain.LeaveRequest
 import com.durys.jakub.leaverequests.request.domain.LeaveRequestRepository
 import com.durys.jakub.leaverequests.request.domain.command.CancelLeaveRequestCommand
+import com.durys.jakub.leaverequests.request.events.LeaveRequestCancelled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Mono
@@ -12,6 +15,7 @@ import reactor.core.publisher.Mono
 @Component
 internal class CancelLeaveRequestCommandHandler(
         private val leaveRequestRepository: LeaveRequestRepository,
+        private val events: Events,
         private val applicantRepository: ApplicantRepository): CommandHandler<CancelLeaveRequestCommand, Mono<OperationResult>> {
 
 
@@ -21,6 +25,23 @@ internal class CancelLeaveRequestCommandHandler(
                 .flatMap {
                     request -> applicantRepository.load(request.applicant())
                         .map { applicant -> applicant.cancel(request) } }
-                .map { OperationResult.success() }
+                .map {
+                    publishEvent(it)
+                    OperationResult.success()
+                }
+    }
+
+    private fun publishEvent(leaveRequest: LeaveRequest) {
+        events.emit(
+                LeaveRequestCancelled(
+                        leaveRequest.acceptant()!!.value,
+                        leaveRequest.type(),
+                        leaveRequest.information().period.from(),
+                        leaveRequest.information().period.to(),
+                        leaveRequest.information().period.days(),
+                        leaveRequest.information().period.hours(),
+                        leaveRequest.information().period.timeFrom(),
+                        leaveRequest.information().period.timeTo()
+                ))
     }
 }
