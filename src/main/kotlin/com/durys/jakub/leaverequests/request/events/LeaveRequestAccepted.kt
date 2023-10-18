f74e60b266305f974dafa8f74561526cbb30ac73
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-18 19:17:30 +0200

    additional fields in leaveRequestAcceptedEvent

--- src/main/kotlin/com/durys/jakub/leaverequests/request/events/LeaveRequestAccepted.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/events/LeaveRequestAccepted.kt
@@ -1,13 +1,17 @@
 package com.durys.jakub.leaverequests.request.events
 
 import com.durys.jakub.leaverequests.cqrs.event.Event
+import com.durys.jakub.leaverequests.request.domain.vo.LeaveRequestType
+import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.util.*
 
-internal class LeaveRequestAccepted(val personId: String, val from: LocalDate, val to: LocalDate,
-                           val timeFrom: LocalTime?, val timeTo: LocalTime): Event {
+internal class LeaveRequestAccepted(val personId: String, val leaveRequestType: LeaveRequestType,
+                           val from: LocalDate, val to: LocalDate,
+                           val days: BigDecimal, val hours: BigDecimal,
+                           val timeFrom: LocalTime?, val timeTo: LocalTime?): Event {
 
 
     private val id: UUID = UUID.randomUUID()
