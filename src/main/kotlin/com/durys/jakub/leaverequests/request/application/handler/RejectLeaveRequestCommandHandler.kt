b525f4ed8e3297e39e01f9af68e07990fd2fa889
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-18 18:18:55 +0200

    RejectLeaveRequestCommandHandler implementation

--- src/main/kotlin/com/durys/jakub/leaverequests/request/application/handler/RejectLeaveRequestCommandHandler.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/request/application/handler/RejectLeaveRequestCommandHandler.kt
@@ -1,18 +1,28 @@
 package com.durys.jakub.leaverequests.request.application.handler
 
+import com.durys.jakub.leaverequests.acceptant.domain.AcceptantRepository
 import com.durys.jakub.leaverequests.common.OperationResult
 import com.durys.jakub.leaverequests.cqrs.command.CommandHandler
 import com.durys.jakub.leaverequests.request.domain.command.RejectLeaveRequestCommand
+import com.durys.jakub.leaverequests.request.domain.flow.FindSentForAcceptationLeaveRequest
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Mono
 
 @Component
-internal class RejectLeaveRequestCommandHandler: CommandHandler<RejectLeaveRequestCommand, Mono<OperationResult>> {
+internal class RejectLeaveRequestCommandHandler(
+        val findSentForAcceptationLeaveRequest: FindSentForAcceptationLeaveRequest,
+        val acceptantRepository: AcceptantRepository): CommandHandler<RejectLeaveRequestCommand, Mono<OperationResult>> {
 
 
     @Transactional
     override fun handle(command: RejectLeaveRequestCommand): Mono<OperationResult> {
-        TODO("Not yet implemented")
+
+        return findSentForAcceptationLeaveRequest.findSentForAcceptation(command.requestId)
+                .flatMap {
+                    request -> acceptantRepository.load(request.acceptant()!!)
+                        .map { acceptant -> acceptant.reject(request, command.rejectionReason) } }
+                .map { OperationResult.success() }
+                .doOnError { OperationResult.error(listOf(it.message!!)) }
     }
 }
