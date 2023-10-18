7147a8df1cb1475d45ff8ca39bf2f63ac25faa47
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-18 20:26:48 +0200

    period factory tests #2

--- src/test/kotlin/com/durys/jakub/leaverequests/request/domain/PeriodFactoryTest.kt
+++ src/test/kotlin/com/durys/jakub/leaverequests/request/domain/PeriodFactoryTest.kt
@@ -58,4 +58,77 @@ class PeriodFactoryTest {
                 .verifyComplete()
     }
 
+    @Test
+    fun createDailyPeriod_shouldSuccessfullyCreateDailyPeriod_whenNotAllDaysAreWorking() {
+
+        val applicantId = ApplicantId(UUID.randomUUID())
+        val requestType = LeaveRequestType.ANNUAL_LEAVE
+        val from = LocalDate.of(2023, 1, 1)
+        val to = LocalDate.of(2023, 1, 3)
+
+        val daysExpected = BigDecimal.valueOf(1)
+        val hoursExpected = BigDecimal.valueOf(8)
+
+        Mockito.`when`(workingTimeScheduleRepository.workingTimeSchedule(applicantId, from, to)).then {
+            Flux.just(
+                    WorkingTimeSchedule(LocalDate.of(2023, 1, 1),
+                            LocalTime.of(8, 0), LocalTime.of(16, 0), false),
+                    WorkingTimeSchedule(LocalDate.of(2023, 1, 2),
+                            LocalTime.of(8, 0), LocalTime.of(15, 0), false),
+                    WorkingTimeSchedule(LocalDate.of(2023, 1, 3),
+                            LocalTime.of(8, 0), LocalTime.of(16, 0), true)
+            )
+        }
+
+        Mockito.`when`(leaveRequestSettlementService.hoursDefinitionRequired(applicantId, requestType, to)).then {
+            Mono.just(false)
+        }
+
+
+        StepVerifier
+                .create(periodFactory.period(applicantId, requestType, from, to, null, null))
+                .consumeNextWith {
+                    assertEquals(daysExpected, it.days())
+                    assertEquals(hoursExpected, it.hours())
+                    assertEquals(from, it.from())
+                    assertEquals(to, it.to())
+                }
+                .verifyComplete()
+    }
+
+    @Test
+    fun createHourlyPeriod_shouldSuccessfullyCreateHourlyPeriod() {
+
+        val applicantId = ApplicantId(UUID.randomUUID())
+        val requestType = LeaveRequestType.ANNUAL_LEAVE
+        val from = LocalDate.of(2023, 1, 1)
+        val to = LocalDate.of(2023, 1, 1)
+        val timeFrom = LocalTime.of(8, 0)
+        val timeTo = LocalTime.of(10, 0)
+
+        val daysExpected = BigDecimal.valueOf(1)
+        val hoursExpected = BigDecimal.valueOf(2)
+
+        Mockito.`when`(workingTimeScheduleRepository.workingTimeSchedule(applicantId, from, to)).then {
+            Flux.just(
+                    WorkingTimeSchedule(LocalDate.of(2023, 1, 1), LocalTime.of(8, 0), LocalTime.of(16, 0), false),
+            )
+        }
+
+        Mockito.`when`(leaveRequestSettlementService.hoursDefinitionRequired(applicantId, requestType, to)).then {
+            Mono.just(true)
+        }
+
+
+        StepVerifier
+                .create(periodFactory.period(applicantId, requestType, from, to, timeFrom, timeTo))
+                .consumeNextWith {
+                    assertEquals(daysExpected, it.days())
+                    assertEquals(hoursExpected, it.hours())
+                    assertEquals(from, it.from())
+                    assertEquals(to, it.to())
+                }
+                .verifyComplete()
+    }
+
 }
