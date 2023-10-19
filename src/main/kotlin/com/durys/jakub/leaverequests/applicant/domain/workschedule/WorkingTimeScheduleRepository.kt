2264aed2f74e25d05a8588365eb6cc69efa3ad09
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-19 18:14:31 +0200

    additional method in workingTimeSchedule

--- src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/workschedule/WorkingTimeScheduleRepository.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/applicant/domain/workschedule/WorkingTimeScheduleRepository.kt
@@ -2,8 +2,10 @@ package com.durys.jakub.leaverequests.applicant.domain.workschedule
 
 import com.durys.jakub.leaverequests.applicant.domain.ApplicantId
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.time.LocalDate
 
 internal interface WorkingTimeScheduleRepository {
     fun workingTimeSchedule(applicantId: ApplicantId, from: LocalDate, to: LocalDate): Flux<WorkingTimeSchedule>
+    fun workingTimeSchedule(applicantId: ApplicantId, at: LocalDate): Mono<WorkingTimeSchedule>
 }
--- src/main/kotlin/com/durys/jakub/leaverequests/applicant/infrastructure/workschedule/RestWorkingTimeScheduleRepository.kt
+++ src/main/kotlin/com/durys/jakub/leaverequests/applicant/infrastructure/workschedule/RestWorkingTimeScheduleRepository.kt
@@ -5,6 +5,7 @@ import com.durys.jakub.leaverequests.applicant.domain.workschedule.WorkingTimeSc
 import com.durys.jakub.leaverequests.applicant.domain.workschedule.WorkingTimeScheduleRepository
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.time.LocalDate
 import java.time.LocalTime
 
@@ -14,4 +15,9 @@ internal class RestWorkingTimeScheduleRepository: WorkingTimeScheduleRepository 
     override fun workingTimeSchedule(applicantId: ApplicantId, from: LocalDate, to: LocalDate): Flux<WorkingTimeSchedule> {
          return Flux.just(WorkingTimeSchedule(LocalDate.now(), LocalTime.now(), LocalTime.now())) //todo
     }
+
+    override fun workingTimeSchedule(applicantId: ApplicantId, at: LocalDate): Mono<WorkingTimeSchedule> {
+        return workingTimeSchedule(applicantId, at, at).single()
+    }
+
 }
